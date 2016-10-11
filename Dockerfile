c2712a6e213877b4b601aaf3e3753194eb73b692
Roberto Huertas <roberto.huertas@outlook.com>
2016-10-11 18:17:49 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.5.2-slim
+MAINTAINER Valudio <development@valudio.com>
+
+ADD ./instantclient /opt/oracle
+
+RUN apt-get update \
+    && apt-get -y install unzip libaio1 curl make g++ \
+    && curl -sL https://deb.nodesource.com/setup_6.x | bash - \
+    && apt-get -y install nodejs
+
+RUN cd /opt/oracle && sh start.sh
+
+EXPOSE 8066
+WORKDIR /opt/app
+
+CMD ["bash"]
---
+++ files/start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "inside oracle folder"
+unzip instantclient-basic-linux.x64-12.1.0.2.0.zip -d /opt/oracle
+unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip -d /opt/oracle
+mv /opt/oracle/instantclient_12_1 /opt/oracle/instantclient
+cd /opt/oracle/instantclient
+ln -s libclntsh.so.12.1 libclntsh.so
+ln -s libocci.so.12.1 libocci.so
+
+export ORACLE_HOME=/opt/oracle/instantclient
+#export LD_LIBRARY_PATH=/opt/oracle/instantclient:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME
+
+export OCI_HOME="/opt/oracle/instantclient"
+export OCI_LIB_DIR="/opt/oracle/instantclient"
+export OCI_INCLUDE_DIR="/opt/oracle/instantclient/sdk/include"
+
+#npm i -g oracledb
